ccb32fe6905ce041e4b95a691a3c4a697f906d59
Joao Lopes <joao.f.c.lopez@gmail.com>
2015-07-04 18:48:24 +0100

    removed some files

--- comp4dbg.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir debug
-cd debug
-../configure --disable-asm  --prefix=/dbg CPPFLAGS=-DDEBUG CXXFLAGS="-g -O0" CFLAGS="-g" CCASFLAGS="-g -O0"
--- comp4pro.bash
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-mkdir profiler
-cd profiler
-../configure --disable-asm  --prefix=/dbg CPPFLAGS= CXXFLAGS="-g -O0" CFLAGS=" -g -DPROFILER" CCASFLAGS=""
